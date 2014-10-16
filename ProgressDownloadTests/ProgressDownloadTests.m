7b923b7b0a47f5d5c4b570a1a04781eb273ba367
492371545 <xmyxumengying@hotmail.com>
2014-10-16 15:44:43 +0800

    Initial Commit

---
+++ ProgressDownloadTests/ProgressDownloadTests.m
@@ -0,0 +1,34 @@
+//
+//  ProgressDownloadTests.m
+//  ProgressDownloadTests
+//
+//  Created by Mengying Xu on 14-10-16.
+//  Copyright (c) 2014年 Crystal Xu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ProgressDownloadTests : XCTestCase
+
+@end
+
+@implementation ProgressDownloadTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
