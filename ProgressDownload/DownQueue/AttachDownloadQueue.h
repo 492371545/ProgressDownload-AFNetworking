c56086561acae00e4ddecff3c15055bd080ec632
492371545 <18551739707@163.com>
2014-10-20 16:03:28 +0800

    Update AttachDownloadQueue.h

--- ProgressDownload/DownQueue/AttachDownloadQueue.h
+++ ProgressDownload/DownQueue/AttachDownloadQueue.h
@@ -8,20 +8,11 @@
 
 #import <Foundation/Foundation.h>
 @class AttachDownloadManager;
-@protocol AttachDownloadQueueDelegate;
 
 @interface AttachDownloadQueue : NSObject
-@property (nonatomic,assign) id<AttachDownloadQueueDelegate>delegate;
 
 - (void)getFileByURL:(NSString *)strURL withProgress:(id)progress;
 - (void)resetListSize:(NSInteger)iSize;
 - (void)removeDownloadFileFromListByURL:(NSString *)strURL;
 - (void)removeAllDownload;
-@end
-
-@protocol AttachDownloadQueueDelegate <NSObject>
-
-//- (void)attachQueue:(AttachDownloadManager *)downlad getFileSucc:(NSData *)filedata fromURL:(NSString *)strURL;
-//- (void)attachQueue:(AttachDownloadManager *)downlad getFileFailedFromURL:(NSString *)strURL;
-
 @end
